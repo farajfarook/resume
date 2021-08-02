5a00626a4c76ae10ad43a0632e1da866e679283c
Faraj Farook <farajfarook@gmail.com>
2021-08-02 23:04:59 +1000

    remove cache on new builds

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
     },
     plugins: [
         new HtmlWebpackPlugin({
